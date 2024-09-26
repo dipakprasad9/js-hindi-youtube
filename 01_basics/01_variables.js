465ce72ddd66e8b0be5b268348b9ea1be687803c
dipakprasad9 <dipakprasad9870@gmail.com>
2024-09-26 11:16:07 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 5441
+let accountEmail = "dipu@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kolkata"
+//don't use var keywords for variables.
+// accountId= 123
+console.log("accountId")
+accountEmail="dipak@gmail.com"
+accountPassword = "54321"
+accountCity="Mumbai"
+console.table([accountId, accountEmail, accountPassword, accountCity])
